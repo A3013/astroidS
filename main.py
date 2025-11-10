44a3b90f0bdf55fb8c50b82fcea66a12dc148d7b
A3013 <abhijeet301303@gmail.com>
2025-11-10 03:03:18 +0000

    starting commit message : Initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+import pygame
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from logger import log_state
+
+def main():
+    print(f"Starting Asteroids! with pygame version: {pygame.__version__}")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+    # Initialize Pygame
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    # Main game loop
+    running = True
+    while running:
+        log_state()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        screen.fill(("black"))
+        pygame.display.flip()
+
+    
+
+
+if __name__ == "__main__":
+    main()
